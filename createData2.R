4a0e1b99d3867dfcab522d2bb339ae33b5da78e7
putaro <tatsumi.cu@gmail.com>
2024-11-22 03:18:26 +0900

    fix

---
+++ createData2.R
@@ -0,0 +1,58 @@
+library(deSolve)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(extraDistr)
+
+# 微分方程式の定義
+Ppun_aggr <- function(t, x, params) {
+  alpha <- params["alpha"]
+  beta <- params["beta"]
+  s <- params["s"]
+  N <- params["N"]
+  
+  dxdt <- (alpha + beta * x[1]) * (N - x[1]) - (s * x[1]) / (s + x[1])
+  
+  list(c(dxdt))
+}
+
+# パラメータの設定
+alpha <- 0.0045
+beta <- 0.00015
+s <- 10
+N <- 600
+times <- seq(0, 100, by = 1)  # 時間範囲
+
+# 初期値をランダムにして10個のデータを生成
+set.seed(100) 
+initial_values <- c(2.0, 100, 200)
+results <- list()
+
+for (i in 1:length(initial_values)) {
+  x0 <- initial_values[i]
+  params <- c(alpha = alpha, beta = beta, s = s, N = N)
+  output <- ode(y = c(x = x0), times = times, func = Ppun_aggr, parms = params)
+  df <- as.data.frame(output)
+  df$initial_value <- x0
+
+  # ベータ二項分布に従ってばらつかせる
+  alpha_beta <- 100  # ベータ分布の形状パラメータ1
+  beta_beta <- 100   # ベータ分布の形状パラメータ2
+  prob <- df$x / N
+  df$x_obs <- rbbinom(n = nrow(df), size = N, alpha = alpha_beta * prob, beta = beta_beta * (1 - prob))
+  df$n_total <- N
+  results[[i]] <- df
+}
+
+# データを結合
+all_data <- bind_rows(results, .id = "series")
+
+# ggplotで散布図を作成
+plt <- ggplot(all_data, aes(x = time, y = x_obs, color = as.factor(series))) +
+  geom_point(size=0.8) +
+  geom_line(aes(y = x), linetype = "solid") +
+  labs(x = "時間", y = "観測個体数", color = "系列") +
+  theme_classic() # +theme(legend.position = "none") 
+
+ggsave("plot2.png", plot = plt, width = 1000, height = 800, units = "px")
+write.csv(all_data[all_data$time>0,], "all_data_2.csv", row.names = FALSE)
